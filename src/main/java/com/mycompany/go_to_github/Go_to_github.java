5673bc35f96f5a328cf274adf2fb12abc24dde26
Bakr <mbakr9212@gmail.com>
2024-09-08 23:08:39 +0300

    First commit

---
+++ src/main/java/com/mycompany/go_to_github/Go_to_github.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.go_to_github;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Go_to_github {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
